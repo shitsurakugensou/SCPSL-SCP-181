3d7c5211e47ce685c82b238e433731e75acfb332
SaigyoujiYuyuko <HGK-SaigyoujiYuyuko@outlook.com>
2019-06-17 21:16:00 +0800

    全局变量类

---
+++ GlobalVar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Smod2;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using Smod2.API;
+
+namespace SCPSL_SCP_181 { 
+
+    /**
+     * 全局变量类
+     */
+    class GlobalVar{
+
+        // scp-181 玩家对象
+        public static Player scp181 = null;
+
+    }
+
+}
