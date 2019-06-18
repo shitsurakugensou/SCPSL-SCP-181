8e286f28ab19e1778b2f8d038be0acfa045a321f
SaigyoujiYuyuko <HGK-SaigyoujiYuyuko@outlook.com>
2019-06-18 13:43:36 +0800

    SCP-181免伤

---
+++ Event/OnPlayerHurtEvent.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Smod2;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using Smod2.API;
+
+namespace SCPSL_SCP_181.Event{
+
+    class OnPlayerHurtEvent : IEventHandlerPlayerHurt{
+
+        private Plugin plugin = null;
+
+        OnPlayerHurtEvent(Plugin plugin){
+            this.plugin = plugin;
+        }
+
+        void IEventHandlerPlayerHurt.OnPlayerHurt(PlayerHurtEvent ev) {
+
+            // 判断玩家是不是SCP-181 不是则退出本次事件
+            if (ev.Player.Name.Equals(GlobalVar.scp181.Name) == false){
+                return;
+            }
+
+            if (ev.Player.PlayerId != GlobalVar.scp181.PlayerId){
+                return;
+            }
+
+
+            /*
+             * 几率判断
+             */
+
+            // 随机数~  1-100 百分比
+            Random random = new Random();
+            int number = random.Next(1, 100);
+
+            // 获取设置的概率
+            int luckyNumber = this.plugin.GetConfigInt("scp181_dodge_chance");
+
+            // 如果随机数<=设置的概率 就免伤害
+            if (number <= luckyNumber) {
+                ev.Damage = 0;
+            }
+            
+            return;
+        }
+
+    }
+
+}
