0ba0e235e011b72d4c3a43b964482c04aad3f376
SaigyoujiYuyuko <HGK-SaigyoujiYuyuko@outlook.com>
2019-06-18 12:05:51 +0800

    修复配置无法读取BUG

--- SCPSL-SCP-181.cs
+++ SCPSL-SCP-181.cs
@@ -8,11 +8,14 @@ using Smod2;
 using Smod2.Attributes;
 
 using SCPSL_SCP_181;
+using SCPSL_SCP_181.Event;
+using Smod2.Config;
 
 namespace SCPSL_SCP_181 {
 
     [PluginDetails(
         author = "SaigyoujiYuyuko",
+        configPrefix = "scp181",
         name = "SCP-181",
         description = "Randomly D-class personnel have randomly chance to open the authorizing door without keycards",
         id = "org.shitsurakugensou.scp.scp-181",
@@ -27,35 +30,37 @@ namespace SCPSL_SCP_181 {
         public override void OnDisable(){
 
             this.Info("[SCP-181][Info] SCP181 disable!");
-            //throw new NotImplementedException();
         }
 
         public override void OnEnable(){
 
             this.Info("[SCP-181][Info] SCP181 loaded!");
-            //throw new NotImplementedException();
         }
 
-        public override void Register(){
+        public override void Register() {
 
             /*
              * 插件配置文件
              */
 
             // 是否启用插件
-            this.AddConfig(new Smod2.Config.ConfigSetting("scp181_enable", true, true, "Is this plugin enable"));
+            this.AddConfig(new ConfigSetting("scp181_enable", true, true, "enable/disable scp181"));
 
             // 181开门几率
-            this.AddConfig(new Smod2.Config.ConfigSetting("scp181_door_open_chance", 6, true, "How many percentage that SCP181 can open the door"));
+            this.AddConfig(new ConfigSetting("scp181_door_open_chance", 6, true, "How many percentage that SCP181 can open the door"));
 
             // 181躲避攻击几率
-            this.AddConfig(new Smod2.Config.ConfigSetting("scp181_dodge_chance", 6, true, "How many percentage that SCP181 can dodge the attack"));
+            this.AddConfig(new ConfigSetting("scp181_dodge_chance", 6, true, "How many percentage that SCP181 can dodge the attack"));
 
             /*
              * 注册事件
              */
 
+            // 开局选取181
             this.AddEventHandlers(new OnGameStart(this));
+
+            // 玩家开门事件
+            this.AddEventHandlers(new OnDoorOpen(this));
 
         }
 
